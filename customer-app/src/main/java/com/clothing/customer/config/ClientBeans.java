2eac7da0c4ba887111215b4ea9a84dd917ecf82c
Ilia Chuvikin <iliachuvikinwork@mail.ru>
2024-12-29 17:21:31 +0300

    Добавил keycloak для customer-app и feedback-service

--- customer-app/src/main/java/com/clothing/customer/config/ClientBeans.java
+++ customer-app/src/main/java/com/clothing/customer/config/ClientBeans.java
@@ -6,35 +6,61 @@ import com.clothing.customer.client.ProductsClientImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ClientBeans {
 
+    // Prototype - делает так, чтобы экземпляр этого бина при каждой вызове создавался снова.
+    // Prototype не внедряется в контекст приложения, поэтому при нужде это нужно делать самостоятельно.
+    // (По умолчанию Singleton)
+    // Здесь можно прописывать общие характеристики для каждого из последующих методов.
+    @Bean
+    @Scope("prototype")
+    public WebClient.Builder atumServicesWebClientBuilder(
+            ReactiveClientRegistrationRepository clientRegistrationRepository,
+            ServerOAuth2AuthorizedClientRepository authorizedClientRepository
+    ) {
+        ServerOAuth2AuthorizedClientExchangeFilterFunction filter =
+                new ServerOAuth2AuthorizedClientExchangeFilterFunction(
+                        clientRegistrationRepository, authorizedClientRepository);
+        filter.setDefaultClientRegistrationId("keycloak");
+        return WebClient.builder()
+                .filter(filter);
+    }
+
     @Bean
     public ProductsClientImpl productsClientImpl(
-            @Value("${atum.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUrl
+            @Value("${atum.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUrl,
+            WebClient.Builder atumServicesWebClientBuilder
     ) {
-        return new ProductsClientImpl(WebClient.builder()
+        return new ProductsClientImpl(atumServicesWebClientBuilder
                 .baseUrl(catalogueBaseUrl)
                 .build());
     }
 
     @Bean
     public FavoriteProductsClientImpl favoriteProductsClientImpl(
-            @Value("${atum.services.feedback.uri:http://localhost:8084}") String feedbackBaseUrl
+            @Value("${atum.services.feedback.uri:http://localhost:8084}") String feedbackBaseUrl,
+            WebClient.Builder atumServicesWebClientBuilder
     ) {
-        return new FavoriteProductsClientImpl(WebClient.builder()
+        return new FavoriteProductsClientImpl(atumServicesWebClientBuilder
                 .baseUrl(feedbackBaseUrl)
                 .build());
     }
 
     @Bean
     public ProductReviewsClientImpl productReviewsClientImpl(
-            @Value("${atum.services.reviews.uri:http://localhost:8084}") String reviewsBaseUrl
+            @Value("${atum.services.reviews.uri:http://localhost:8084}") String reviewsBaseUrl,
+            WebClient.Builder atumServicesWebClientBuilder
     ) {
-        return new ProductReviewsClientImpl(WebClient.builder()
+        return new ProductReviewsClientImpl(atumServicesWebClientBuilder
                 .baseUrl(reviewsBaseUrl)
                 .build());
     }
+
 }
