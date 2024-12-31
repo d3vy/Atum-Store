e134fc283d142e4af97a42fb8b658829fec31529
Ilia Chuvikin <iliachuvikinwork@mail.ru>
2024-12-31 17:46:28 +0300

    К админ сервису не подключен только customer-app

--- customer-app/src/main/java/com/clothing/customer/config/ClientBeans.java
+++ customer-app/src/main/java/com/clothing/customer/config/ClientBeans.java
@@ -3,10 +3,15 @@ package com.clothing.customer.config;
 import com.clothing.customer.client.FavoriteProductsClientImpl;
 import com.clothing.customer.client.ProductReviewsClientImpl;
 import com.clothing.customer.client.ProductsClientImpl;
+import de.codecentric.boot.admin.client.config.ClientProperties;
+import de.codecentric.boot.admin.client.registration.ReactiveRegistrationClient;
+import de.codecentric.boot.admin.client.registration.RegistrationClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
@@ -61,6 +66,26 @@ public class ClientBeans {
         return new ProductReviewsClientImpl(atumServicesWebClientBuilder
                 .baseUrl(reviewsBaseUrl)
                 .build());
+    }
+
+    @Bean
+    public RegistrationClient registrationClient(
+            ClientProperties clientProperties,
+            ReactiveClientRegistrationRepository clientRegistrationRepository,
+            ReactiveOAuth2AuthorizedClientService authorizedClientService
+    ) {
+
+        ServerOAuth2AuthorizedClientExchangeFilterFunction filter =
+                new ServerOAuth2AuthorizedClientExchangeFilterFunction(
+                        new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
+                                clientRegistrationRepository, authorizedClientService)
+                );
+        filter.setDefaultClientRegistrationId("keycloak");
+
+        return new ReactiveRegistrationClient(WebClient.builder()
+                .filter(filter)
+                .build(),
+                clientProperties.getReadTimeout());
     }
 
 }
